f27afba24d6ef8e653888f77b05d68bae4d43b89
Siti Zahra Ananda Kurniawan <162027699+szahraak@users.noreply.github.com>
2024-12-25 19:27:47 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -914,7 +914,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch(`http://wa-logger-back.vercel.app/api/send-file`, {
+                const response = await fetch(`https://wa-logger-back.vercel.app/api/send-file`, {
                     method: "POST",
                     body: formData,
                 });
